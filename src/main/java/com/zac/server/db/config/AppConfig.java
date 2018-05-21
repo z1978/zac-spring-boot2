41bafc7c6fbd26a7a0b8263608e436bae32ca832
zac <zgithub@hotmail.com>
2018-05-21 19:59:25 +0900

    test

---
+++ src/main/java/com/zac/server/db/config/AppConfig.java
@@ -0,0 +1,46 @@
+package com.zac.server.db.config;
+
+import javax.sql.DataSource;
+
+import org.seasar.doma.SingletonConfig;
+import org.seasar.doma.jdbc.Config;
+import org.seasar.doma.jdbc.dialect.Dialect;
+import org.seasar.doma.jdbc.dialect.MysqlDialect;
+import org.seasar.doma.jdbc.tx.LocalTransactionDataSource;
+import org.seasar.doma.jdbc.tx.LocalTransactionManager;
+import org.seasar.doma.jdbc.tx.TransactionManager;
+
+@SingletonConfig
+public class AppConfig implements Config {
+
+    private static final AppConfig INSTANCE = new AppConfig();
+
+    private final Dialect dialect;
+    private final LocalTransactionDataSource dataSource;
+    private final TransactionManager transactionManager;
+
+    private AppConfig() {
+        dialect = new MysqlDialect();
+        dataSource = new LocalTransactionDataSource("jdbc:mysql://localhost:3306/test", "root", "hogehoge");
+        transactionManager = new LocalTransactionManager(dataSource.getLocalTransaction(getJdbcLogger()));
+    }
+
+    @Override
+    public Dialect getDialect() {
+        return dialect;
+    }
+
+    @Override
+    public DataSource getDataSource() {
+        return dataSource;
+    }
+
+    @Override
+    public TransactionManager getTransactionManager() {
+        return transactionManager;
+    }
+
+    public static AppConfig singleton() {
+        return INSTANCE;
+    }
+}
